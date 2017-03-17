462d05d5f29d440ce98a81db395d6c5941b315ec
Matthew Campbell <mjc4648@gmail.com>
2017-03-17 18:07:18 -0400

    Finished full VCAP_SERVICES parse, but tests aren't working yet

--- osgi-vcap-config/src/main/java/com/snowfort/osgi/cloud/config/vcap/VcapServicesProperties.java
+++ osgi-vcap-config/src/main/java/com/snowfort/osgi/cloud/config/vcap/VcapServicesProperties.java
@@ -1,7 +1,7 @@
 package com.snowfort.osgi.cloud.config.vcap;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
@@ -9,8 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Dictionary;
+import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.Map;
 
 public class VcapServicesProperties {
 
@@ -42,34 +42,46 @@ public class VcapServicesProperties {
 
             // iterate over all VCAP_SERVICES properties and if it's a JsonPrimitive (leaf node),
             // create a flattened vcap.services.* config property based on the value.
-            JsonObject vcapServices = new JsonParser().parse(vcapServicesJson).getAsJsonObject();
-            vcapServices.entrySet().stream().forEach(entry -> );
+            JsonElement vcapServices = new JsonParser().parse(vcapServicesJson);
+            addJsonPropertiesAsConfig(configProps, "vcap.services", vcapServices);
+
+            Enumeration<String> keys = configProps.keys();
+            while(keys.hasMoreElements()){
+                String key = keys.nextElement();
+                logger.info("KEY:  " + key + ",   VALUE:  " + configProps.get(key));
+            }
 
 
 
-
-
-
-            String jdbcUrl = var("cleardb[0].credentials.jdbcUrl");
-            logger.info("Setting vcap.services.mysql.jdbcUrl to " + jdbcUrl);
-            configProps.put("mysql.jdbcUrl", jdbcUrl);
-
-            String username = var("cleardb[0].credentials.username");
-            logger.info("Setting vcap.services.mysql.username to " + username);
-            configProps.put("mysql.username", username);
-
-            String password = var("cleardb[0].credentials.password");
-            logger.info("Setting vcap.services.mysql.password to " + password);
-            configProps.put("mysql.password", password);
-
-            config.update(configProps);
-            logger.info("config:  " + config.toString());
+//            String jdbcUrl = var("cleardb[0].credentials.jdbcUrl");
+//            logger.info("Setting vcap.services.mysql.jdbcUrl to " + jdbcUrl);
+//            configProps.put("mysql.jdbcUrl", jdbcUrl);
+//
+//            String username = var("cleardb[0].credentials.username");
+//            logger.info("Setting vcap.services.mysql.username to " + username);
+//            configProps.put("mysql.username", username);
+//
+//            String password = var("cleardb[0].credentials.password");
+//            logger.info("Setting vcap.services.mysql.password to " + password);
+//            configProps.put("mysql.password", password);
+//
+//            config.update(configProps);
+//            logger.info("config:  " + config.toString());
         }
     }
 
-    private void addConfig(Dictionary<String, Object> configProps, Map.Entry<String, JsonElement> element, String parentPath) {
-        if (element.getValue().isJsonPrimitive()) {
-            configProps.put(parentPath+"."+element.getKey(), element.getValue().getAsJsonPrimitive().getAsString());
+    private void addJsonPropertiesAsConfig(Dictionary<String, Object> configProps, String elementPath, JsonElement element) {
+        if (element.isJsonPrimitive()) {
+            configProps.put(elementPath, element.getAsJsonPrimitive().getAsString());
+        } else if (element.isJsonObject()) {
+            element.getAsJsonObject().entrySet().stream().forEach(entry -> {
+                addJsonPropertiesAsConfig(configProps, elementPath+"."+entry.getKey(), entry.getValue());
+            });
+        } else if (element.isJsonArray()) {
+            JsonArray array = element.getAsJsonArray();
+            for (int i=0; i < array.size(); i++) {
+                addJsonPropertiesAsConfig(configProps, elementPath+"["+i+"]", array.get(i));
+            }
         }
     }
 
